0b4fc42718d2ab794d774217329ec3928f02fa9c
CJ-The-Black-Child <congomigue@gmail.com>
2024-01-11 18:08:22 +0300

    Checks if password is valid: Implementation of an is_valid function that expects 2 arguments and returns a boolean.

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,5 +10,12 @@ def hash_password(password: str) -> bytes:
     Hashes a password using a random salt.
     """
     salt = bcrypt.gensalt()
-    hashed = bcrypt(password.encode(), salt)
+    hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if a hashed password was formed from the given password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
