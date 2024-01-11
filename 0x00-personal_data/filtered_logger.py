2fae8acfc03c38eb62264bd3c0371ea22f6e4954
CJ-The-Black-Child <congomigue@gmail.com>
2024-01-11 13:04:26 +0300

    Log formatter task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,8 @@ Filtering logs
 """
 import re
 from typing import List
+import logging
+
 
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
@@ -14,3 +16,23 @@ def filter_datum(
     for field in fields:
         message = re.sub(f"{field}=[^{separator}]*", f"{field}={redaction}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+
+    def __init__(self, fields):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+        self.pattern = re.compile(f'({"|".join(fields)})=[^;]*')
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        return self.pattern.sub(r"\1=" + self.REDACTION, message)
+
+
+if __name__ == "__main__":
+    main()
