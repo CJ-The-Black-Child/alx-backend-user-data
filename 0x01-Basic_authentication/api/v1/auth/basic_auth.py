125b4fff71bf840af2dd16eaff348ae0d2cfc620
CJ-The-Black-Child <congomigue@gmail.com>
2024-01-16 11:34:00 +0300

    Basic - Base64 decode: Add the method def decode_base64_authorization_header  that returns the decoded value of a Base64 string base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 BasicAuth module for the API
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -15,3 +16,23 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(" ")[1]
+    
+    
+    def decode_base64_authorization_header(
+        self,base64_authorization_header: str
+    ) -> str:
+        """
+        Method to decode the Base64 Authorization
+        """
+        if (
+            base64_authorization_header is None or type(
+                base64_authorization_header
+            ) is not str
+        ):
+            return None
+        try:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            message_bytes = base64.b64decode(base64_bytes)
+            return message_bytes.decode('utf-8')
+        except Exception:
+            return None
