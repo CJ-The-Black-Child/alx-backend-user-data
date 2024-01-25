f6f379be1b76c242eadeacc690be6ed8484ac00d
CJ-The-Black-Child <congomigue@gmail.com>
2024-01-25 10:32:04 +0300

    updated the User feature functionality

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+This module defines the User model for the database.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    Represents a record from the users table.
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the User instance.
+
+        Returns:
+            str: A string representation of the User instance.
+        """
+        return (
+            f"< User(
+                email='{self.email}',
+                hashed_password='{self.hashed_password}', "
+                f"session_id='{self.session_id}',
+                reset_token='{self.reset_token}'
+            )
+            >"
+        )
